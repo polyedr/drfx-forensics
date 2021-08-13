626551c32dc02fac9a4aebad13f389040ab8a5d9
admin <admin@localhost.localdomain>
2021-08-13 21:54:26 +0700

    Making a REST API for the smart cropping

--- drfx/urls.py
+++ drfx/urls.py
@@ -35,10 +35,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("api.urls")),
     path("api-auth/", include("rest_framework.urls")),
-    path(
-        "api/v1/color_augmenters/",
-        include("color_augmenter.urls", namespace="color_augmenters"),
-    ),
+    # path(
+    #     "api/v1/color_augmenters/",
+    #     include("color_augmenter.urls", namespace="color_augmenters"),
+    # ),
     path(
         "api/v1/smartcrops/",
         include("smartcropsize.urls", namespace="smartcrops"),
